9ca9688ad3701aef4fb61ca35c2af4b5ded8370f
er84yaabhishek <er84yaabhishek@gmail.com>
2025-12-28 12:31:50 +0530

    feat: add core models, APIs, and Tailwind setup

--- store/views.py
+++ store/views.py
@@ -1,8 +1,16 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Product,Category
+from .serializers import ProductSerializer,CategorySerializer
 
-# Create your views here.
-def home(request):
-    data = {
-        'message': 'Welcome to Soft84ya'
-    }
-    return JsonResponse(data)
+@api_view(['GET'])
+def get_products(req):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_categories(req):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
